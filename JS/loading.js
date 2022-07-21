016ff680607c1e26ec8b1ff3553db707fbf162f1
Hyungseob Kim <supremepilot@naver.com>
2022-07-21 17:04:26 +0900

    loading

---
+++ JS/loading.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    console.log("Loaded");
+    $('.loader6').hide();
+    $("body").removeAttr("style");
+    $(".div-container").css("visibility","visible");
+    console.log("End");
+});
