99c9c161b3fc28b0b7a8b1a74c32577c37c790c2
Hyungseob Kim <supremepilot@naver.com>
2022-07-22 18:42:44 +0900

    push

--- JS/loading.js
+++ JS/loading.js
@@ -3,5 +3,21 @@ $(document).ready(function () {
     $('.loader6').hide();
     $("body").removeAttr("style");
     $(".div-container").css("visibility","visible");
+    $(document).bind('keydown',function(e){
+        if ( e.keyCode == 123 /* F12 */) {
+            e.preventDefault();
+            e.returnValue = false;
+        }
+    });
     console.log("End");
 });
+
+// document.onkeydown = function(e) {
+//     if(e.keyCode === 123 || 
+//       (e.ctrlKey && e.shiftKey && e.keyCode == 73) || 
+//       (e.ctrlKey && e.shiftKey && e.keyCode == 74) || 
+//       (e.ctrlKey && e.shiftKey && e.keyCode == 67)) {
+//         e.preventDefault();
+//         e.returnValue = false;
+//     }
+// };
